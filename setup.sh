d5ba732d1a767b382df27cdebe3667546a486232
jim-haughey1990 <jim.haughey1990@gmail.com>
2020-07-30 17:26:38 +0100

    added some updates

--- setup.sh
+++ setup.sh
@@ -10,18 +10,34 @@ function check_helm(){
 
 function install_cf_operator(){
   echo " --- installing cf operator --- "
-  kubectl create namespace cf-operator
-  helm repo \
-    add quarks https://cloudfoundry-incubator.github.io/quarks-helm/
-  helm install quarks/cf-operator -n cf-operator --generate-name
+  kubectl create namespace kubecf
+  kubectl config set-context --current --namespace=kubecf
+  helm repo update
+  if [[ $(helm status cf-operator  || false) ]]; then
+    echo "cf-operator already exists updating ..."
+    HELM_VERB="upgrade"
+  else 
+    HELM_VERB="install"
+  fi
+  helm ${HELM_VERB} cf-operator \
+    --namespace kubecf quarks/cf-operator
+
 }
 
 function install_kubecf(){
   echo " --- installing kubecf --- "
-  helm install kubecf \
+  export MINIKUBE_IP=$(minikube ip)
+  envsubst < ./assets/kubecf/deployment.yml > ./assets/kubecf/my_values.yml
+  if [[ $(helm status kubecf || false) ]]; then
+    echo "kubecf already exists updating ..."
+    HELM_VERB="upgrade"
+  else 
+    HELM_VERB="install"
+  fi
+  helm ${HELM_VERB} kubecf \
     --namespace kubecf \
-    --values ./assets/kubecf/deployment.yml \
-    https://github.com/SUSE/kubecf/releases/download/v0.2.0/kubecf-0.2.0.tgz
+    --values ./assets/kubecf/my_values.yml \
+    https://github.com/cloudfoundry-incubator/kubecf/releases/download/v2.3.0/kubecf-v2.3.0.tgz
 }
 
 check_helm
